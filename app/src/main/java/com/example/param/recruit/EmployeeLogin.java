6229e264219b27221863481e3dcee653b5f05d52
PMangal98 <parammangal@gmail.com>
2019-04-12 18:10:33 +0530

    Fixed employee login so that it works with android.

--- app/src/main/java/com/example/param/recruit/EmployeeLogin.java
+++ app/src/main/java/com/example/param/recruit/EmployeeLogin.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.JsonReader;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
@@ -12,6 +13,8 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -21,9 +24,8 @@ import okhttp3.Response;
 public class EmployeeLogin extends Activity {
     EditText usernameEditText;
     EditText passwordEditText;
+    String url = "http://10.0.2.2/recruitlink/backend/login_employee.php";
     public static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
-    OkHttpClient client = new OkHttpClient();
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,29 +42,53 @@ public class EmployeeLogin extends Activity {
                 + "}";
     }
 
-    String post(String url, String json) throws IOException {
-        RequestBody body = RequestBody.create(JSON, json);
+//    String post(String url, String json) throws IOException {
+//        RequestBody body = RequestBody.create(JSON, json);
+//        Request request = new Request.Builder()
+//                .url(url)
+//                .post(body)
+//                .build();
+//        try (Response response = client.newCall(request).execute()) {
+//            return response.body().string();
+//        }
+//    }
+
+    public void postRequest(String postUrl, String postBody){
+        OkHttpClient client = new OkHttpClient();
+        RequestBody body = RequestBody.create(JSON, postBody);
+
         Request request = new Request.Builder()
-                .url(url)
+                .url(postUrl)
                 .post(body)
                 .build();
-        try (Response response = client.newCall(request).execute()) {
-            return response.body().string();
-        }
-    }
 
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                if(e != null)
+                    e.printStackTrace();
+                call.cancel();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Log.d("TAG", response.body().string());
+            }
+        });
+    }
     public void loginClicked(View v){
         String username = usernameEditText.getText().toString();
         String password = passwordEditText.getText().toString();
 
-        String loginJson = postData(username, password);
-        try {
-            String response = post("http://localhost/recruitlink/backend/login_employee.php", loginJson);
-            System.out.println(response);
-        }catch(IOException e){
-            e.printStackTrace();
-        }
+//        String loginJson = postData(username, password);
+//        try {
+//            String response = post("http://10.0.0.2/recruitlink/backend/login_employee.php", loginJson);
+//            System.out.println(response);
+//        }catch(IOException e){
+//            e.printStackTrace();
+//        }
 
+        postRequest(url, postData(username, password));
 //        JSONObj
 //        JsonReader jsonReader = new JsonReader();
 //        public static String url_login_employee = "http://localhost/recruitlink/backend/login_employee.php";
